5a95415a696ded9df1adb96a6072a0a279ac43f1
RRaycheva <68399907+RRaycheva@users.noreply.github.com>
2021-01-08 09:32:27 +0200

    Missing log model

---
+++ BookStoreApi/BookStoreApi/Models/Common/LogEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BookStoreApi.Log
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+        public DateTime LoggingTime { get; set; }
+        public string Ip { get; set; }
+        public string RequestVerb { get; set; }
+        public string Message { get; set; }
+    }
+}
